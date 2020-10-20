679b0ff6b17ccff907a09b4d0cb9e6bf704a6af6
Domidoodle <domidoodle@localhost.localdomain>
2020-10-20 14:49:35 +1300

    Minor Changes Removed grid.css from nav.php Corrected favicon.ico url

--- nav.php
+++ nav.php
@@ -5,12 +5,11 @@ include "processor.php";
 ?>
 <head>
   <title>LANGFORGE</title>
-  <link rel="stylesheet" href="css/grid.css">
   <link rel="stylesheet" href="css/main.css">
   <script src="js/main.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link rel="icon" type="image/png" href="icons/favicon-192x192.png" sizes="192x192">
-  <link rel="shortcut icon" href="favicon.ico">
+  <link rel="shortcut icon" href="icons/favicon.ico">
 </head>
 
 <body>
